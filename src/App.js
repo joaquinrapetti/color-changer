6035a9b6d60e0ee8cbb12eaadfc63bb6879192af
joaquinrapetti <j.rapetti92@gmail.com>
2022-04-04 13:17:56 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "./components/Input";
+import Square from "./components/Square";
 
 function App() {
-  return <div className="App">Holis</div>;
+  const [colorValue, setColorValue] = useState("");
+  const [hexValues, setHexValues] = useState("");
+  const [isDarkText, setIsDarkText] = useState(true);
+
+  return (
+    <div className="App">
+      <Square
+        colorValue={colorValue}
+        hexValues={hexValues}
+        isDarkText={isDarkText}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValues={setHexValues}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
+      />
+    </div>
+  );
 }
 
 export default App;
